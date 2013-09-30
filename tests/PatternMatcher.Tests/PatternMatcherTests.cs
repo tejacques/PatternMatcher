79c729104aef89e7461183e764bbe07dc8291719
Tom Jacques <tejacques@gmail.com>
2013-09-30 00:57:26 -0400

    Add tests.

---
+++ tests/PatternMatcher.Tests/PatternMatcherTests.cs
@@ -0,0 +1,14 @@
+﻿using Functional.PatternMatching;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMatcher.Tests
+{
+    public class PatternMatcherTests
+    {
+    }
+}
